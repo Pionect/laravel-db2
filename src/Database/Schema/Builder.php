80a0825c804b0872835660e593232b1727e00f5d
Vincent van Hoven <vincent@pionect.nl>
2025-04-16 15:44:17 +0200

    [FIX] Remove call on non-existing method

--- src/Database/Schema/Builder.php
+++ src/Database/Schema/Builder.php
@@ -63,12 +63,9 @@ class Builder extends \Illuminate\Database\Schema\Builder
             $table,
         ]);
 
-        $res = $this->connection->getPostProcessor()
-                                ->processColumnListing($results);
-
         return array_values(array_map(function($r) {
             return $r->column_name;
-        }, $res));
+        }, $results));
     }
 
     /**
